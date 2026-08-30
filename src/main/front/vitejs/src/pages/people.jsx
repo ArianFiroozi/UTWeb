a584f0e99f5909a4058b013755959189fbcd37a8
ArianFiroozi <arianfiroozi3@gmail.com>
2026-08-30 22:53:42 +0330

    feat: edit people

--- src/main/front/vitejs/src/pages/people.jsx
+++ src/main/front/vitejs/src/pages/people.jsx
@@ -10,6 +10,7 @@ import sepehr_baz from "../assets/people/sepehr_baz.jpg";
 import bardia from "../assets/people/bardia.jpg";
 import mahsa from "../assets/people/mahsa.jpg";
 import hanita from "../assets/people/hanita.jpg";
+import faezeh from "../assets/people/faezeh.jpg"
 import "./people.css";
 
 const PLACEHOLDER_IMAGE = "https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png";
@@ -23,9 +24,9 @@ const MEMBERS = [
       { name: "Sohrab Moradi", role: "Advisor - Ex Lead", photo: sohrab },
       { name: "M. Sepehr Bazargan", role: "Qt Developer", photo: sepehr_baz },
       { name: "Sepehr Jamali", role: "Qt Developer", photo: sepehr_jam },
+      { name: "Faezeh Misaghi", role: "Hardware Engineer", photo: faezeh },
       { name: "Arian Firoozi", role: "AI - Decision Making", photo: "https://arianfiroozi.github.io/assets/img/prof_pic-480.webp" },
       { name: "Sobhan Abedi", role: "AI - Decision Making", photo: sobhan },
-      { name: "Erfan Daraee", role: "Hardware - CV", photo: erfan },
       { name: "Mani Hosseini", role: "AI - Decision Making", photo: "https://media.licdn.com/dms/image/v2/D4E03AQGrt7Z4ifdEOw/profile-displayphoto-scale_400_400/B4EZx6ENfzIEAk-/0/1771574465789?e=1781740800&v=beta&t=6qpH_nLLLV-3DKqjwdaUzpjBq-yTHCSACvO96G3pgxE" },
       { name: "Mahdi Yari", role: "AI - Decision Making", photo: "https://media.licdn.com/dms/image/v2/D5603AQHYP0zWds6ONw/profile-displayphoto-scale_400_400/B56ZmahJjaKMAg-/0/1759234007330?e=1781740800&v=beta&t=nd8EcybfoG11y-Zca5GeTDV2LpyAdy6rj1DTzmx1dX8" },
     ]
@@ -34,6 +35,7 @@ const MEMBERS = [
     sectionKey: "Former Members",
     people: [
       { name: "Mohammad Reza M. Hashemi", role: "AI Researcher", photo: PLACEHOLDER_IMAGE },
+      { name: "Erfan Daraee", role: "Hardware - CV", photo: erfan },
       { name: "Amirhossein Kahrobaeian", role: "Researcher", photo: PLACEHOLDER_IMAGE },
       { name: "Hanita Nik Nasab", role: "Researcher", photo: hanita },
       { name: "Amirreza Kaffashan", role: "Researcher", photo: PLACEHOLDER_IMAGE },
